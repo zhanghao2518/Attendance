4a98faf0657a699c4565d74bd7b54f563111d541
wlqlq_18 <352977670@qq.com>
2020-11-19 15:37:01 +0800

    前端上传

---
+++ front-end/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+import personalAttendance from '@/components/personal/personal_attendance'
+import personalVocation from '@/components/personal/personal_vocation'
+import teamAttendance from '@/components/team/team_attendance'
+import teamVocation from '@/components/team/team_vocation'
+import teamStatistics from "@/components/team/team_statistics"
+
+
+export default new Router({
+  routes: [
+      {
+          path: '/',
+          redirect: '/team_attendance'
+      },{
+          path: '/team_attendance',
+          component: teamAttendance
+      }, {
+          path: '/team_vocation',
+          component: teamVocation
+      }, {
+          path: '/team_statistics',
+          component: teamStatistics
+      }, {
+        path: '/personal_attendance',
+        component: personalAttendance
+      }, {
+        path: '/personal_vocation',
+        component: personalVocation
+      }
+  ]
+})
