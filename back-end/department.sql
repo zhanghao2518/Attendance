d1524e3edc63eb0b6ec86f7e5ed026283594d370
zhanghao <2411410566@qq.com>
2020-12-06 16:27:19 +0800

    张豪+2020/12/06+完整项目

---
+++ back-end/department.sql
@@ -0,0 +1,5 @@
+INSERT INTO `department` VALUES (0, '前端开发');
+INSERT INTO `department` VALUES (1, '后端开发');
+INSERT INTO `department` VALUES (2, '算法');
+INSERT INTO `department` VALUES (3, '人力资源');
+INSERT INTO `department` VALUES (4, '人事部');
